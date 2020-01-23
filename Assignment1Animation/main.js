d4e697ed995a7b45066661007e1a65e7c2924861
naydarnj15 <58826342+naydarnj15@users.noreply.github.com>
2020-01-23 13:12:50 -0800

    Add files via upload

---
+++ Assignment1Animation/main.js
@@ -0,0 +1,118 @@
+var AM = new AssetManager();
+
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
+    this.spriteSheet = spriteSheet;
+    this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.scale = scale;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
+
+    ctx.drawImage(this.spriteSheet,
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth * this.scale,
+                 this.frameHeight * this.scale);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+// no inheritance
+function Background(game, spritesheet) {
+    this.x = 0;
+    this.y = 0;
+    this.spritesheet = spritesheet;
+    this.game = game;
+    this.ctx = game.ctx;
+};
+
+Background.prototype.draw = function () {
+    this.ctx.drawImage(this.spritesheet,
+                   this.x, this.y);
+};
+
+Background.prototype.update = function () {
+};
+
+function GreenGuy(game, spritesheet) {
+    this.animation = new Animation(spritesheet, 110, 230, 6, 0.2, 12, true, 1);
+    this.x = 0;
+    this.y = 0;
+    this.speed = 203;
+    this.game = game;
+    this.ctx = game.ctx;
+}
+
+GreenGuy.prototype.draw = function () {
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+}
+
+GreenGuy.prototype.update = function () {
+    if (this.animation.elapsedTime < this.animation.totalTime * 8 / 14)
+        this.x += this.game.clockTick * this.speed;
+    if (this.x > 510) this.x = -230;
+}
+
+
+function Fire(game, spritesheet) {
+    this.animation = new Animation(spritesheet, 128, 128, 4, 0.24, 8, false, 0.5);
+    this.speed = 0;
+    this.ctx = game.ctx;
+    Entity.call(this, game, 330, 150);
+}
+
+Fire.prototype = new Entity();
+Fire.prototype.constructor = Fire;
+
+Fire.prototype.update = function () {
+    this.x += this.game.clockTick * this.speed;
+    if (this.x > 800) this.x = -230;
+    Entity.prototype.update.call(this);
+}
+
+Fire.prototype.draw = function () {
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
+}
+
+AM.queueDownload("./images/greenguy2.png");
+AM.queueDownload("./images/fireSprite.png");
+AM.queueDownload("./images/hardwood.jpg");
+
+AM.downloadAll(function () {
+    var canvas = document.getElementById("gameWorld");
+    var ctx = canvas.getContext("2d");
+
+    var gameEngine = new GameEngine();
+    gameEngine.init(ctx);
+    gameEngine.start();
+
+    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./images/hardwood.jpg")));
+	gameEngine.addEntity(new Fire(gameEngine, AM.getAsset("./images/fireSprite.png")));
+    gameEngine.addEntity(new GreenGuy(gameEngine, AM.getAsset("./images/greenguy2.png")));
+
+    console.log("All Done!");
+});
